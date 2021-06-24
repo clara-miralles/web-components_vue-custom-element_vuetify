d2b02705c5084fe65460802b6b1deb52b2032b76
Clara Miralles <clara.miralles.imperial@gmail.com>
2021-06-24 11:04:58 +0200

    vue add vuetify + deleted vuetify and vue stuff

---
+++ src/components/first-component.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    Hola!
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'FirstComponent',
+
+    data: () => ({
+  
+    }),
+  }
+</script>
---
+++ src/eewc-app.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-main>
+      <FirstComponent/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import FirstComponent from './components/first-component';
+
+export default {
+  name: 'App',
+
+  components: {
+    FirstComponent,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
