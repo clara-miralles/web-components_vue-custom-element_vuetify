c6578ddcb25c6f22648148579b91de9a9eb28509
Clara Miralles <clara.miralles.imperial@gmail.com>
2021-06-28 11:27:16 +0200

    web core component side menu in

--- src/components/eewc-navigation-sidebar.vue
+++ src/components/eewc-navigation-sidebar.vue
@@ -1,12 +1,34 @@
 <template>
-    <v-list>
-        <v-list-item
-          v-for="menu in menus"
-          :key="menu.title"
-        >
-        {{menu}}
+  <v-navigation-drawer
+    :mini-variant="$vuetify.breakpoint.smAndDown"
+    mini-variant-width="40"
+    permanent
+    class="side-navigation-drawer"
+    floating
+    width="250"
+  >
+
+  <v-divider v-if="!$vuetify.breakpoint.smAndDown && showName"></v-divider>
+    
+    <v-list class="pa-0">
+      <v-list-item
+        :ripple="false"
+        v-for="(menu, index) in menus"
+        class="side-menu mb-0 pa-0"
+        :class="{ 'side-menu-selected-item': menu.endpoint === currentEndpoint }"
+        tile
+        :key="index"
+        @click="$emit('itemSelected', menu)"
+      >
+        <v-list-item-icon class="item-icon">
+          <v-icon size="14" class="primary--text" v-html="menu.icon"></v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title class="primary--text menu-text text-uppercase">{{ menu }}</v-list-item-title>
+        </v-list-item-content>
       </v-list-item>
     </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -21,7 +43,7 @@ export default {
   },
   data(){
     return {
-      
+
     }
   }
  
@@ -29,5 +51,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.side-menu-selected-item {
+  background-color: #eeeeee;
+  font-weight: 600;
+  border-radius: 0 !important;
+}
+.side-navigation-drawer {
+  top: 70px !important;
+}
+.item-icon {
+  width: 50px;
+  margin: auto !important;
+  i {
+    margin: auto;
+  }
+}
+.menu-text {
+  letter-spacing: 1px !important;
+  font-size: 14px !important;
+}
+.side-menu:hover {
+  width: 102%;
+  border-radius: 0 !important;
+}
+.side-menu:focus {
+  background-color: transparent;
+  width: 102%;
+  border-radius: 0 !important;
+}
 </style>
+
+
